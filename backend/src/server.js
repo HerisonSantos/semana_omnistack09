58bb5b09dfaa5e2329ad05effe2b38f726dc3975
HerisonSantos <50580649+HerisonSantos@users.noreply.github.com>
2020-01-29 08:41:42 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-jxhrd.mongodb.net/semana09?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://herison10:herison10@omnistack-jxhrd.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
